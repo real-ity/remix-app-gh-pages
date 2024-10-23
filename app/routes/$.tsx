8c7ab38f47d42ffd05ef7e0eab4cede5ee2b4d46
Joseph Scherreik <joe.scherreik@gmail.com>
2024-10-22 23:53:46 -0400

    Create $.tsx

---
+++ app/routes/$.tsx
@@ -0,0 +1,15 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useRouteError } from "@remix-run/react";
+import ErrorFallback from "~/components/ErrorFallback";
+export async function clientLoader({ request }: LoaderFunctionArgs) {
+  throw new Response(`${new URL(request.url).pathname} Not Found`, {
+    status: 404,
+  });
+}
+export default function CatchAllPage() {
+  return null;
+}
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return <ErrorFallback {...{ error }} />;
+}
