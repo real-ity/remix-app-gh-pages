cd6458e90551a8ae9da659014d0847886c937482
Joseph Scherreik <joe.scherreik@gmail.com>
2024-10-23 18:21:31 -0400

    update formatting

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
-        v3_throwAbortReason: true, 
+        v3_throwAbortReason: true,
         // v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
